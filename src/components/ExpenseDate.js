b7f55949ecd40153f85a24e4774998c9f4c02cc5
sumiliholden <sumiliholden@gmail.com>
2022-01-26 03:39:31 +0800

    test commit

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -5,6 +5,7 @@ function ExpenseDate(props) {
   const year = props.date.getFullYear();
   return (
       <div className='expense-date'>
+        test
         <div className='expense-date__month'>{month}</div>
         <div className='expense-date__year'>{year}</div>
         <div className='expense-date__day'>{day}</div>
